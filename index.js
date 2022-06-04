ec7f20b8f68664cb7bbfc09ead194dcb59fa662e
hiw-devnet <105480529+hiw-devnet@users.noreply.github.com>
2022-06-04 15:38:06 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function() {
-  function execCopy() {
+  function tplCopy() {
     // テキストエリアのテキストを取得（コピーさせるテキスト）
-    var text = $('pre').text();
+    var text = $('#tpl pre').text();
     // コピーする媒体となるテキストエリアを生成
     var clipboard = $('<textarea></textarea>');
     clipboard.text(text);
@@ -16,5 +16,26 @@ $(function() {
     // 通知
     alert("コードがクリップボードにコピーされました");
   }
-  $('code div button').on('click', execCopy);
+  $('#tpl div button').on('click', tplCopy);
+});
+
+$(function() {
+  function loadCopy() {
+    // テキストエリアのテキストを取得（コピーさせるテキスト）
+    var text = $('#load pre').text();
+    // コピーする媒体となるテキストエリアを生成
+    var clipboard = $('<textarea></textarea>');
+    clipboard.text(text);
+    // body直下に一時的に挿入
+    $('body').append(clipboard);
+    // 選択状態にする
+    clipboard.select();
+    // WebExtension APIのブラウザ拡張の仕組みを呼び出しクリップボードにコピー
+    document.execCommand('copy');
+    // 不要なテキストエリアを削除
+    clipboard.remove();
+    // 通知
+    alert("コードがクリップボードにコピーされました");
+  }
+  $('#load div button').on('click', loadCopy);
 });
